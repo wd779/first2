a4b5d6e4b35ec63510be5073aa1a75d204a99ceb
HuangRenJie0808 <m18843108067@163.com>
2020-10-28 21:02:30 +0800

    路由更新

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,15 @@ const routes = [
       { path: 'home', name: 'Home', component: Home },//首页
       { path: 'mine', name: 'mine', component: () => import('../views/mine.vue') }, // 我的
       { path: 'Appointment', name: 'Appointment', component: () => import('../views/Appointment.vue') }, // 课程
-      { path: "practice", name: "Practice", component: () => import("../views/practice/Practice.vue"), },//   练习页面
-      { path: "point", name: "point", component: () => import("../views/practice/point.vue") }, //考点练习
-      { path: "paper_package", name: "paper_package", component: () => import("../views/practice/paper_package.vue") }, //套卷联系
-      { path: "exam", name: "exam", component: () => import("../views/practice/exam.vue") },  // 仿真模考
-      { path: "error_ques", name: "error_ques", component: () => import("../views/practice/error_ques.vue") },// 错题练习
-      { path: "assess", name: "assess", component: () => import("../views/practice/assess.vue") }, // 测评记录
-      { path: "ques_collect", name: "ques_collect", component: () => import("../views/practice/ques_collect.vue") },// 习题收藏
+      { path: "practice", name: "Practice", component: () => import("../views/practice/Practice.vue"), },// 练习页面主页面-zhy
+      { path: "point", name: "point", component: () => import("../views/practice/point.vue") }, //考点练习-zhy
+      { path: "paper_package", name: "paper_package", component: () => import("../views/practice/paper_package.vue") }, //套卷练习-zhy
+      { path: "exam", name: "exam", component: () => import("../views/practice/exam.vue") }, // 仿真模考-zhy
+      { path: "error_ques", name: "error_ques", component: () => import("../views/practice/error_ques.vue") },// 错题练习-zhy
+      { path: "assess", name: "assess", component: () => import("../views/practice/assess.vue") }, // 测评记录-zhy
+      { path: "ques_collect", name: "ques_collect", component: () => import("../views/practice/ques_collect.vue") },// 习题收藏-zhy
+      { path: "shu", name: "shu", component: () => import("../views/practice/shu.vue") },// 学习跳转页面-zhy
+      { path: "order", name: "order", component: () => import("../views/order/order.vue") },// 约课记录主页面-zhy
     ]
   }, // 主页面
   { path: '/login', name: 'Login', component: () => import('../views/login/Login.vue') }, // 密码登录
@@ -34,6 +36,7 @@ const routes = [
   { path: '/message', name: 'Message', component: () => import('../views/my/Message.vue') }, // 我的消息
   { path: '/feedback', name: 'Feedback', component: () => import('../views/my/Feedback.vue') }, // 意见反馈
   { path: '/options', name: 'Options', component: () => import('../views/my/Options.vue') }, // 设置
+
   { path: '/study', name: 'study', component: () => import('../views/Home/Study.vue') },// 学习日历
   { path: '/fudao', name: 'fudao', component: () => import('../views/Home/FuDao.vue') },// 一对一辅导页面,
 ]
