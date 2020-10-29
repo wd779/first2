7cfea966f2ad8a70e2255fda4105ceeb397a3870
张彤彤 <zhangqilingatong@163.com>
2020-10-29 21:30:50 +0800

    新增路由，更新添加弹出信封页面

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
-
     <router-view></router-view>
-    
+    <div class="box1" @click="$router.push('/email')">
+      <div class="lo">
+        <label for class="icon">
+          <span type="call-button-1" width="32px" height="32px" class="base-icon"></span>
+        </label>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,12 +26,39 @@ body,
   height: 100%;
   list-style: none;
   font-size: 0.16rem;
-  background-color: #F0F2F5;
+  background-color: #f0f2f5;
 }
 * {
   margin: 0;
   padding: 0;
 }
 
-  
+.box1 {
+  height: 0.5rem;
+  width: 0.5rem;
+  background-color: rgb(0, 122, 255);
+  border-radius: 50%;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 5px 14px;
+  text-align: center;
+  cursor: pointer;
+  position: fixed;
+  z-index: 999;
+  top:0.5rem;
+
+}
+.lo {
+  padding-top: 0.14rem;
+}
+.icon {
+  display: inline-block;
+  vertical-align: middle;
+}
+.base-icon {
+  vertical-align: middle;
+  padding-right: 0.32rem;
+  height: 0.3rem;
+  display: inline-block;
+  background: url(https://static.meiqia.com/widget/static/icon-mq-round@2x.png) -0.32rem -0.03rem /
+    0.64rem no-repeat;
+}
 </style>
--- src/views/my/Vip.vue
+++ src/views/my/Vip.vue
@@ -1,5 +1,5 @@
 <template>
-<div>
+  <div>
     <!-- 头部组件 -->
     <app-header>
       <template>
@@ -7,39 +7,171 @@
         <div slot="right"></div>
       </template>
     </app-header>
-</div>
+    <!-- 会员 -->
+    <div class="top">
+      <div class="box">
+        <img
+          src="https://msmk2019.oss-cn-shanghai.aliyuncs.com/uploads/image/20191HHDExgz0u1567065946.png"
+          alt
+        />
+        <div class="info">
+          <div class="name">玛卡巴卡</div>
+          <p class="note">开通会员免费学习</p>
+        </div>
+      </div>
+    </div>
+    <!-- 购买会员 -->
+    <div data-v-d183123a class="bottom">
+      <p data-v-d183123a>
+        <span data-v-d183123a class="tag1">购买会员</span>
+        <span data-v-d183123a class="tag2">购买会员后可免费观看会员课程</span>
+      </p>
+      <div data-v-d183123a class="vip-items">
+        <div data-v-d183123a class="vip-item select">
+          <p data-v-d183123a class="name">季度会员</p>
+          <p data-v-d183123a class="price">
+            <img
+              data-v-d183123a
+              src="https://msmk2019.oss-cn-shanghai.aliyuncs.com/uploads/image/20191HHDExgz0u1567065946.png"
+              class="price-ico"
+            />
+            <span data-v-d183123a>40.00</span>
+          </p>
+        </div>
+        <div data-v-d183123a class="vip-item">
+          <p data-v-d183123a class="name">半年会员</p>
+          <p data-v-d183123a class="price">
+            <img
+              data-v-d183123a
+              src="https://msmk2019.oss-cn-shanghai.aliyuncs.com/uploads/image/20191HHDExgz0u1567065946.png"
+              class="price-ico"
+            />
+            <span data-v-d183123a>75.00</span>
+          </p>
+        </div>
+        <div data-v-d183123a class="vip-item">
+          <p data-v-d183123a class="name">月会员</p>
+          <p data-v-d183123a class="price">
+            <img
+              data-v-d183123a
+              src="https://msmk2019.oss-cn-shanghai.aliyuncs.com/uploads/image/20191HHDExgz0u1567065946.png"
+              class="price-ico"
+            />
+            <span data-v-d183123a>15.00</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import appHeader from "../../components/AppHeader.vue";
 export default {
- // 组件名称
- name: '',
- // 组件参数 接收来自父组件的数据
- props: [],
- // 局部注册的组件
- components: {appHeader},
- // 组件状态值
- data () {
- return {}
- },
- // 计算属性
- computed: {},
- // 侦听器
- watch: {},
- // 组件方法
- methods: {},
- /**
- * 组件实例创建完成，属性已绑定，但DOM还未生成，$ el属性还不存在
- */
- created () {
- },
- mounted () {
- },
-}
+  // 组件名称
+  name: "",
+  // 组件参数 接收来自父组件的数据
+  props: [],
+  // 局部注册的组件
+  components: { appHeader },
+  // 组件状态值
+  data() {
+    return {};
+  },
+  // 计算属性
+  computed: {},
+  // 侦听器
+  watch: {},
+  // 组件方法
+  methods: {},
+  /**
+   * 组件实例创建完成，属性已绑定，但DOM还未生成，$ el属性还不存在
+   */
+  created() {},
+  mounted() {}
+};
 </script> 
 
 <style scoped lang="scss">
-
-
+.top {
+  position: relative;
+}
+.box {
+  border-top: 1px solid #f5f5f5;
+  height: 19.46667vw;
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+  padding: 0 4vw;
+  position: relative;
+}
+img {
+  width: 11.46667vw;
+  //   height: 11.46667vw;
+  border-radius: 50%;
+}
+.info {
+  margin-left: 2.66667vw;
+  line-height: 5.6vw;
+}
+.name {
+  font-size: 3.73333vw;
+  font-weight: 500;
+  color: #595959;
+}
+.note {
+  font-size: 3.2vw;
+  color: #8c8c8c;
+}
+.bottom {
+  padding: 8vw 4vw;
+}
+.select[data-v-d183123a] {
+  border: 0.26667vw solid #e6c37f;
+  background-color: #f9f0d9;
+}
+.vip-items {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  align-content: space-between;
+  padding-top: 5.33333vw;
+}
+.vip-item {
+  width: 26.66667vw;
+  height: 29.33333vw;
+  border-radius: 2.13333vw;
+  border: 0.26667vw solid #d9d9d9;
+  background-color: #fff;
+  margin-bottom: 2.66667vw;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.tag1 {
+  font-size: 4vw;
+  font-weight: 500;
+  color: #595959;
+}
+.tag2 {
+  margin-left: 2.66667vw;
+  font-size: 3.2vw;
+  color: #8c8c8c;
+}
+.name {
+  font-size: 3.46667vw;
+  font-weight: 600;
+  color: #595959;
+  margin-bottom: 2.66667vw;
+  line-height: 4vw;
+}
+.price {
+  font-size: 7.46667vw;
+  color: #d5a322;
+}
+.price-ico {
+  width: 3.73333vw;
+  margin-right: 0.4vw;
+}
 </style>
