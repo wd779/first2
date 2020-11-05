e39e720cfba13f11bab4713a1b6527d63213f4ed
张彤彤 <zhangqilingatong@163.com>
2020-11-05 20:57:00 +0800

    课程详情Details评论渲染，点击“回访”进行路由传参

--- src/views/Appointment/Details.vue
+++ src/views/Appointment/Details.vue
@@ -10,18 +10,8 @@
         <div class="ping">
           <p>{{ data.title }}</p>
           <p>
-            <van-icon
-              name="star-o"
-              size="24"
-              v-if="data.is_collect == 0"
-              @click="liang"
-            />
-            <van-icon
-              name="star"
-              size="24"
-              v-if="data.is_collect != 0"
-              @click="liang"
-            />
+            <van-icon name="star-o" size="24" v-if="data.is_collect == 0" @click="liang" />
+            <van-icon name="star" size="24" v-if="data.is_collect != 0" @click="liang" />
           </p>
         </div>
 
@@ -29,7 +19,7 @@
         <p>共{{ data.total_periods }}课时 | {{ data.sales_num }}人已报名</p>
         <p>
           开课时间 &nbsp;{{
-            new Date(data.created_at * 1000).toLocaleDateString()
+          new Date(data.created_at * 1000).toLocaleDateString()
           }}
         </p>
       </div>
@@ -42,35 +32,69 @@
       </div>
       <div class="box">
         <p>课程介绍</p>
-        <div class="connect">
-          {{ data.course_statement }}
-        </div>
+        <div class="connect">{{ data.course_statement }}</div>
       </div>
       <div class="box">
         <p>课程大纲</p>
-        <div class="li" v-for="item in 10" :key="item">
+        <div class="li" v-for="item in 10" :key="item" @click="$router.push({path:'/MyStudy',query:{vid:$route.query.id}})">
           <span class="dian">·</span>
           <van-tag type="warning">回放</van-tag>
           <span class="title">第一讲第一课时</span>
           <p>
             {{ data.address == undefined ? "团队" : data.address }}
+            
             {{ new Date(data.created_at * 1000).toLocaleDateString() }}
-            -
+             - 
             {{ new Date(data.created_at * 1000).toLocaleDateString() }}
           </p>
         </div>
       </div>
+      <!-- 课程评论 -->
+      <div class="cd_title">
+        <p>课程评论</p>
+        <ul class="black">
+          <li v-for="item in list" :key="item.id">
+            <div class="he">
+              <div class="blues">
+                <img :src="item.avatar" alt />
+              </div>
+              <div class="evaluate-t-left">
+                <div class="info0">
+                  <span>{{item.nickname}}</span>
+                  <span>
+                    <van-rate
+                      v-model="item.grade"
+                      :size="8"
+                      color="#ff976a"
+                      void-icon="star"
+                      void-color="#eee"
+                    />
+                    <!-- {{item.grade}} -->
+                  </span>
+                  <span class="times">{{item.created_at*1000 |timer}}</span>
+                </div>
+                <div class="evaluate-btom">{{item.content}}</div>
+              </div>
+            </div>
+          </li>
+        </ul>
+        <!-- <p v-for="item in list" :key="item.id">{{item.created_at*1000 |timer}}</p> -->
+      </div>
     </div>
+    <!-- 按钮 -->
     <div class="btn">
-      <van-button type="default" class="btn_n" @click="ToStudy"
-        >立即学习</van-button
-      >
+      <van-button type="default" class="btn_n" @click="ToStudy">立即学习</van-button>
     </div>
   </div>
 </template>
 
 <script>
-import { GetCurriculum, SignUp, GetComment,GetOutline } from "../../utils/appointmtemtApi";
+import {
+  GetCurriculum,
+  SignUp,
+  GetComment,
+  GetOutline
+} from "../../utils/appointmtemtApi";
 import { postCollect, postCancel } from "../../utils/homeApi";
 export default {
   // 组件名称
@@ -82,12 +106,13 @@ export default {
       data: "",
       id: "",
       info: "",
+      list: []
     };
   }, // 计算属性
   computed: {
     isbuy() {
       return this.data.is_join_study;
-    },
+    }
   }, // 侦听器
   watch: {}, // 组件方法
   methods: {
@@ -95,7 +120,7 @@ export default {
       console.log(this.$route.query.id);
       let res = await SignUp({
         shop_id: this.$route.query.id,
-        type: 5,
+        type: 5
       });
       if (res.code == 200) {
         this.$toast("报名成功");
@@ -108,7 +133,7 @@ export default {
     },
     async getOutline() {
       let obj = {
-        id: String(this.$route.query.id),
+        id: String(this.$route.query.id)
       };
       let { data } = await GetOutline(obj);
       console.log(data);
@@ -117,10 +142,11 @@ export default {
       let obj = {
         page: 1,
         limit: 10,
-        id: String(this.$route.query.id),
+        id: String(this.$route.query.id)
       };
       let { data } = await GetComment(obj);
       console.log(data);
+      this.list = data.list;
     },
     // outline
     ToStudy() {
@@ -162,7 +188,7 @@ export default {
     async onpostCollect() {
       let res = await postCollect({
         course_basis_id: this.$route.query.id,
-        type: 1,
+        type: 1
       });
 
       if (res.code == 200) {
@@ -178,7 +204,7 @@ export default {
         this.getdata(); //重新渲染
       }
       console.log(res);
-    },
+    }
   },
   /**
    * 组件实例创建完成，属性已绑定，但DOM还未生成，$ el属性还不存在
@@ -190,7 +216,7 @@ export default {
     // this.GetCurriculum().then(res=>{
     //     console.log(res);
     // })
-  },
+  }
 };
 </script> 
 
@@ -259,13 +285,19 @@ export default {
 }
 .box .li {
   margin: 0.03rem 0;
+  padding: 2.66667vw 0 0 0;
 }
 .box .li p {
   text-indent: 2em;
   color: #8c8c8c;
+  font-size: 3.2vw;
+  padding-left: 8.66667vw;
+  padding-bottom: 2.33333vw;
+  padding-top: 1vw;
 }
 .box .li .title {
   margin-left: 0.2rem;
+  font-size: 3.2vw;
 }
 .address {
   width: 0.5rem;
@@ -283,5 +315,62 @@ export default {
 }
 .connect {
   padding: 0.2rem 0;
+}
+// 课程评论
+.cd_title {
+  width: 100%;
+  margin-top: 0.2rem;
+  background: white;
+  padding: 0 0.2rem;
+  box-sizing: border-box;
+}
+.black li {
+  display: flex;
+  padding: 2.66667vw 2.66667vw;
+}
+.black img {
+  width: 6.66667vw;
+  height: 6.66667vw;
+  border-radius: 50%;
+}
+.evaluate-t-left {
+  flex: 1;
+}
+.info0 {
+  display: flex;
+  padding-left: 2.66667vw;
+  align-items: center;
+  span {
+    font-size: 3.73333vw;
+    font-weight: 400;
+    color: #333;
+  }
+}
+.he:last-child {
+  border: 0;
+  display: flex;
+}
+.van-rate {
+  margin-left: 2.66667vw;
+  margin-top: 0;
+  height: 4vw;
+  box-sizing: border-box;
+  user-select: none;
+}
+.times {
+  flex: 1;
+  font-size: 2.93333vw;
+  font-weight: 400;
+  color: #999;
+  text-align: right;
+  padding-left: 4vw;
+}
+.evaluate-btom {
+  padding-top: 1.33333vw;
+  line-height: 6.4vw;
+  color: #999;
+  font-size: 3.2vw;
+  font-weight: 400;
+  margin-left: 2.66667vw;
 }
 </style>
